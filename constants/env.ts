c846a0f7f32d293c38e65a21335cd18c5b49d9f3
Larry Barrios Jr <github@larrybarriosjr.com>
2022-09-18 09:00:57 +0800

    Configure dotenv-vault

---
+++ constants/env.ts
@@ -0,0 +1,6 @@
+export const SITE_URL = process.env.SITE_URL
+export const BASE_URL = process.env.BASE_URL
+export const FALLBACK_URL = process.env.FALLBACK_URL
+export const CURRENCY_URL = process.env.CURRENCY_URL
+export const FILE_FORMAT_1 = process.env.FILE_FORMAT_1
+export const FILE_FORMAT_2 = process.env.FILE_FORMAT_2
