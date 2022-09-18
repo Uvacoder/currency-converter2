61064faa6c3f1adeaa54d137be790254f4b19cd8
Larry Barrios Jr <github@larrybarriosjr.com>
2022-09-18 09:34:09 +0800

    Create list of countries with semantic ui flag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import type { AppProps } from "next/app"
+import { ThemeProvider } from "@emotion/react"
+import { AppProps } from "next/app"
+import Head from "next/head"
+import "semantic-ui-css/semantic.min.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+export default function App(props: AppProps) {
+	const { Component, pageProps } = props
+
+	return (
+		<>
+			<Head>
+				<title>Currency Converter</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	)
 }
 
-export default MyApp
+const theme = {
+	colors: {
+		control: {
+			cardBg: "#f8f8f8",
+			cardBorder: "#d3d3d3",
+			button: "#c0c0c0",
+		},
+	},
+}
